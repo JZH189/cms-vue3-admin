dcf29c222d2ec32d63562a5bb2e4b2d4a19b9e13
Zhihua Jiang <vn544q7@email.wal-mart.com>
2023-08-08 10:39:06 +0800

    feat: 去掉i18n的配置

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import path from 'path-browserify';
-import { isExternal } from '@/utils/index';
-import AppLink from './Link.vue';
+import path from "path-browserify";
+import { isExternal } from "@/utils/index";
+import AppLink from "./Link.vue";
 
-import { translateRouteTitleI18n } from '@/utils/i18n';
-import SvgIcon from '@/components/SvgIcon/index.vue';
+import SvgIcon from "@/components/SvgIcon/index.vue";
 
 const props = defineProps({
   /**
@@ -12,7 +11,7 @@ const props = defineProps({
    */
   item: {
     type: Object,
-    required: true
+    required: true,
   },
 
   /**
@@ -20,8 +19,8 @@ const props = defineProps({
    */
   basePath: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
 const onlyOneChild = ref(); // 临时变量，唯一子路由
@@ -53,7 +52,7 @@ function hasOneShowingChild(children = [], parent: any) {
 
   // 2：如果无子路由, 复制当前路由信息作为其子路由，满足只拥有一个子路由的条件，所以返回 true
   if (showingChildren.length === 0) {
-    onlyOneChild.value = { ...parent, path: '', noShowingChildren: true };
+    onlyOneChild.value = { ...parent, path: "", noShowingChildren: true };
     return true;
   }
   return false;
@@ -92,7 +91,7 @@ function resolvePath(routePath: string) {
             :icon-class="onlyOneChild.meta.icon"
           />
           <template #title>
-            {{ translateRouteTitleI18n(onlyOneChild.meta.title) }}
+            {{ onlyOneChild.meta.title }}
           </template>
         </el-menu-item>
       </app-link>
@@ -105,9 +104,7 @@ function resolvePath(routePath: string) {
           v-if="item.meta && item.meta.icon"
           :icon-class="item.meta.icon"
         />
-        <span v-if="item.meta && item.meta.title">{{
-          translateRouteTitleI18n(item.meta.title)
-        }}</span>
+        <span v-if="item.meta && item.meta.title">{{ item.meta.title }}</span>
       </template>
 
       <sidebar-item
