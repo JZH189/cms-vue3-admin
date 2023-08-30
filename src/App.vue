f4ca4eb3435c77c4b6bc28f9c445d11d9fe48729
Zhihua Jiang <vn544q7@email.wal-mart.com>
2023-08-30 10:45:02 +0800

    feat: 修改ts配置，修改element-plus为中文

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 import { ElConfigProvider } from "element-plus";
 import { useAppStore } from "@/store/modules/app";
 const appStore = useAppStore();
 </script>
 
 <template>
-  <el-config-provider :size="appStore.size">
+  <el-config-provider :size="appStore.size" :locale="zhCn">
     <router-view />
   </el-config-provider>
 </template>
