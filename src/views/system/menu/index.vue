78c04d883786e9b4662ee773d2fd11cb7c354d2b
victorjiang <victorjiang189@163.com>
2023-09-05 00:44:43 +0800

    feat: 菜单查询

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,53 +1,42 @@
 <script lang="ts" setup>
-import SuperTable, { FormItemType } from "@/components/SuperTable";
+import SuperTable from "@/components/SuperTable";
+import listToTree from "@/utils/listToTree"
+import { hasPermission } from "@/utils/hasPermission"
 
-const formData = [
-  {
-    label: "菜单或权限名称：",
-    key: "name",
-    type: FormItemType.input,
-    value: undefined,
-  },
-];
+const res = hasPermission("/sys/perm/menu/updateMenu")
+console.log('res: ', res);
 
-function customRow(data) {
-  //type 0=目录 1=菜单 2=权限
-  const level1 = data.map((item) => {
-    if (item.type === 0) {
-      return {
-        ...item,
-        children: [],
-      };
-    }
-  });
-  const level2 = data.filter((item) => {
-    if (item.type === 1) {
-      return {
-        ...item,
-        children: [],
-      };
-    }
-  });
-  const level3 = data.filter((item) => item.type === 2);
-  level1.forEach((element) => {
-    // if()//todo
-  });
-}
 </script>
 
 <template>
   <SuperTable
     query-api="/admin/sys/perm/menu/menuList"
-    :form-data="formData"
-    :label-width="125"
-    :custom-row="customRow"
+    no-form
+    row-key="id"
+    :custom-row="listToTree"
   >
-    <el-table-column label="菜单名称" prop="userId"></el-table-column>
-    <el-table-column label="类型" prop="ip"></el-table-column>
-    <el-table-column label="图标" prop="ip"></el-table-column>
-    <el-table-column label="路由" prop="ip"></el-table-column>
-    <el-table-column label="视图路径" prop="ip"></el-table-column>
-    <el-table-column label="权限" prop="ip"></el-table-column>
-    <el-table-column label="排序" prop="sort"></el-table-column>>
+    <el-table-column label="菜单名称" prop="name"></el-table-column>
+    <el-table-column label="类型" prop="type">
+      <template #default="{row}">
+        <el-button v-if="row.type === 0" type="primary" size="small">目录</el-button>
+        <el-button v-if="row.type === 1" type="success" size="small">菜单</el-button>
+        <el-button v-if="row.type === 2" type="danger" size="small">权限</el-button>
+      </template>
+    </el-table-column>
+    <el-table-column label="图标" prop="icon">
+      <template #default="{row}">
+        <i :class="`i-${row.icon}`"></i>
+      </template>
+    </el-table-column>
+    <el-table-column label="路由" prop="router"></el-table-column>
+    <el-table-column label="视图路径" prop="viewPath"></el-table-column>
+    <el-table-column label="权限" prop="perms"></el-table-column>
+    <el-table-column label="排序" prop="sort"></el-table-column>
+    <el-table-column label="操作" fixed="right" width="140px">
+      <template #default="{row}">
+        <el-button type="primary" size="small" plain>编辑</el-button>
+        <el-button type="danger" size="small" plain>删除</el-button>
+      </template>
+    </el-table-column>
   </SuperTable>
 </template>
